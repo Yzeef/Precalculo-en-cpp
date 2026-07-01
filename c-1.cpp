1e1a89af802b9a331ae7548392d95eb473d0c326
Yzeef <yzeefofficial@gmail.com>
2026-06-30 22:36:43 -0300

    paneo-general

--- c-1.cpp
+++ c-1.cpp
@@ -6,7 +6,21 @@ int main(int argc, char** argv) {
 	
 	 cout<<"HOlaaa"<<endl;
 	 
-	
+	 // Diseñar un programa que permite leer Altura de 3 Alumnoss 
+	 //Diseñar un programa que  grafique una lineal
+	 //Primero, el modelo es mx+b=y
+	 //para que sea mas facil digerir por parte del usuario
+	//tiene que tener nombres amigables, 
+	//ejemplo mb puede ser personas asistentes q asistiran al evento
+	//y m puyede ser las personas que se van despues de cada hora
+	//osea la constante de decrecimiento
+	//Para que este numero no acceda a los negativos, debe haber un condicional que frene antes de llegar a lo negativos
+	//porque no existen asistidores negativos
+	//cnatidad de gente asistiran: cuantas perrsonas se van por hora x
+
+	//EJEMPLO:
+	//asistiran 200 personas a la boda y se iran 20 por hora una vez sean las 23
+	//
 	
 	
 	
