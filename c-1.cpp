6ed3680ff156229751f00da45cc316af79cf78db
Yzeef <yzeefofficial@gmail.com>
2026-06-30 23:47:40 -0300

    Hace los calculos, pero hay que rehacer Algunas partes

--- c-1.cpp
+++ c-1.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <cstring>
 
 int main(int argc, char** argv) {
-	
 	 cout<<"HOlaaa"<<endl;
 	 
 	 // Diseñar un programa que permite leer Altura de 3 Alumnoss 
@@ -22,6 +21,61 @@ int main(int argc, char** argv) {
 	//asistiran 200 personas a la boda y se iran 20 por hora una vez sean las 23
 	//
 	
+	
+	int people;//Personas
+	int peopleDegree;//Pendiente
+	int horaPastel;//Hora inicial
+	int horaX;//Lo usaremos de Iterador
+	int horaFinal=1;//Hora donde se almacena
+	int ResetCheck;
+	char HoraDN[3]="PM";
+	int test=0;
+
+	cout<<"Ingrese la cantidad de personas que asistiran:";
+	cin>> people;
+
+	cout<<"ingrese la cantidad de personas que se iran por hora";
+	cin>>peopleDegree;
+
+	cout<<"Ingrese la hora a la cual cortan la torta(Pm)";
+	cin>>horaPastel;
+	
+	//Nose que paso
+	while (test>0){   
+		while (people-20*horaX==0){
+			
+			horaX++;
+
+		}
+		test=people-20*horaX++;
+		}
+	horaFinal=horaPastel+horaX;
+
+
+
+	if (horaFinal>24)
+		{
+			horaFinal=horaFinal-24;
+			ResetCheck++;
+		
+			if (strcmp(HoraDN,"PM")==0)
+			{
+				strcpy(HoraDN,"AM");
+			}
+			else{
+				strcpy(HoraDN,"PM");
+			
+			// necesitamos 1: se reste 24 horas
+			//switchee a Am y Pm, empieza en pm
+			}
+				 
+		}
+	
+	cout<<"Aqui ya se veria todo";
+	cout<<"A las "<<horaFinal<<" Se fueron todos los Que asistieron al evento"<<endl;
+
+	//Solo falta defininir, una estrucutura para corroborar que este bien hecho
+	//La hora es ultra importante
 	
 	
 	return 0;
